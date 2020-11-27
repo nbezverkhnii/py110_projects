797fa88a6efb22b2c7e21dd2a5905d8263ae7e5f
nikolay24534 <nikolaybezverhny@gmail.com>
2020-11-27 12:19:02 +0300

    Внес предложенные преподавателем исправления

--- tic_tac_toe_test.py
+++ tic_tac_toe_test.py
@@ -1,31 +1,39 @@
 from typing import List, Set
 
+# Размер поля игры в Крестики-Нолики 3х3
+SIZE = 3
+
 def print_board(matrix: List[str]) -> None:
     """
     Выводит на экран игровое поле. Игровое поле - multi-line строка.
     В таком виде код легко масштабируется для поля любого размера больше 3
-    
-    d (dimension) - длина строки игрового поля
-    
+       
     row_list содержит строки матрицы (например, 3х3):
         [matrix[0:3], 
          matrix[3:7], 
          matrix[6:9]]
          
-    [''] добавляется для праивльной отрисовки игравого поля в консоли
-    Отдельные ячейки отделяются друг от друга симоволм "|"
+    Отдельные ячейки при отрисовке отделяются друг от друга симоволм "|"
     
     Parameters
     ----------
     matrix: List[str]
         Список, заполненный "X" или "О", или " "
+
+    >>> print_board(['', ' ', ' ', ' ', '','', ' ', ' ', ' ', '','', ' ', ' ', ' ', ''])
+    >>> | | | |
+        | | | |
+        | | | |
     """
-    d = 3
+    global SIZE
     
-    row_list = [matrix[i*d:(i+1)*d] for i in range(d)] #row_list[0] = [' ', ' ', ' ']
-    row_list = list(map(lambda x: [''] + x + [''], row_list)) #row_list[0] = ['', ' ', ' ', ' ', '']
+    row_list = [matrix[i*SIZE:(i+1)*SIZE] for i in range(SIZE)] #row_list[0] = [' ', ' ', ' ']
+
+    # [''] добавляется для праивльной отрисовки игравого поля в консоли
+    row_list = list(map(lambda x: [''] + x + [''], row_list)) 
     
     board = ''
+    # reversed - чтобы строка с номерами ячеек 1,2,3 оказалсь внизу
     for row in reversed(row_list):
         board += '|'.join(row) + '\n'
  
@@ -39,10 +47,10 @@ def input_value(user: str, last_input: Set[str]) -> int:
     Выполняется проверка введенного пользователем числа:
     - точно ли пользователь ввел число, а не букву
     - лежит ли введенное пользователем число в диапазоне 1-9 (для игры 3х3)
+    (допустимые значения для ввода содержатся в cheak_list)
     - не ввел ли пользователь позицию, которая была занята ранее
     
     Если все проверки прошли прошли успешно, то число приводится к типу int.
-    cheak_list преобразован к типу set, чтобы быстрее осуществлять поиск элементов
     
     Parameters
     ----------
@@ -54,25 +62,29 @@ def input_value(user: str, last_input: Set[str]) -> int:
     Returns
     -------
     number: int
-        Число 1-9, введенное пользователем.
+        Число, введенное пользователем.
     
     """
+    global SIZE
+    
     number = input(f'Пользователь {user}, введите число ')
-    cheak_list = set([str(i) for i in range(1, 10)]) # {'1','2',...} 
+    # x*x быстрее, чем x**2
+    # преобразован к типу set, чтобы быстрее осуществлять поиск элементов
+    cheak_list = set([str(i) for i in range(1,SIZE*SIZE+1)])
     
     
-    def is_digit(number):
+    def is_not_digit(number):
         return not number.isdigit()
    
 
-    def ch_list(number):
+    def not_in_chack_list(number):
         return not number in cheak_list
     
     
-    def is_last(number):
+    def in_last_input(number):
         return int(number) in last_input
     
-    while is_digit(number) or ch_list(number) or is_last(number):
+    while is_not_digit(number) or not_in_chack_list(number) or in_last_input(number):
         number = input('Вы ввели что-то не то. \nВведите снова: ')
         
     return int(number)
@@ -83,8 +95,7 @@ def cheak_win(user: str, board_matrix: List[str], step: int) -> bool:
     Проверка победы.
 
     Выполняется проверка условий победы для строк, столбцов и диагоналей игрового поля.
-    Выиграть можно не раньше, чем на 5 ходу, поэтому if step > 4
-
+    
     Parameters
     ----------
     user: str
@@ -100,21 +111,27 @@ def cheak_win(user: str, board_matrix: List[str], step: int) -> bool:
         Если победа - True, если нет - False.
 
     """
-    win_combination = [user]*3 #['X', 'X', 'X'] или ['O', 'O', 'O']  
+    global SIZE
+    
+    # Победа - кобимнация одинаковых символов длиной SIZE
+    win_combination = [user]*SIZE #['X', 'X', 'X'] 
     win_case_list = []
     
-    for i in range(3):
-        win_case_list.append(board_matrix[i*3:(i+1)*3] == win_combination) # строки
-        win_case_list.append(board_matrix[i:9:3] == win_combination) # столбцы
-        
-    win_case_list.append(board_matrix[0:9:4] == win_combination) # диагонали
-    win_case_list.append(board_matrix[2:7:2] == win_combination) # диагонали
-        
-    if step > 4 and any(win_case_list):
+    for i in range(SIZE):
+        win_case_list.append(board_matrix[i*SIZE:(i+1)*SIZE] == win_combination) # строки
+        win_case_list.append(board_matrix[i:SIZE*SIZE:SIZE] == win_combination) # столбцы
+    
+    # У двумерной матрицы две диагонали
+    main_diagonal = board_matrix[0:SIZE*SIZE:SIZE+1] == win_combination
+    antidiagonal = board_matrix[SIZE-1:SIZE*SIZE-2:SIZE-1] == win_combination   
+    win_case_list.append(main_diagonal)
+    win_case_list.append(antidiagonal)
+    
+    result = False
+    # Выиграть можно не раньше, чем сделано SIZE+1 ходов    
+    if step > SIZE+1 and any(win_case_list):
         print('\n' + f'Пользователь {user} выиграл!!!' + '\n')
         result = True
-    else:
-        result = False
     
     return result
 
@@ -122,50 +139,50 @@ def cheak_win(user: str, board_matrix: List[str], step: int) -> bool:
 def cheak_pass(step: int, win: bool) -> None:
     """
     Проверка на ничью.
+    Ничья ничья наступает, когда ходы уже закончились, 
+    а победа ни одного из пользователей не наступила
 
     Parameters
     ----------
     step: int
         Номер игрового хода
     win: bool
-        Была ли победа на последнем ходу 
+        Была ли победа на сделанном ходу 
     """
-    if step == 9 and not win:
-        print('Ничья!' + '\n')
+    global SIZE
+
+    if step == SIZE*SIZE and not win:
+        print('Ничья!\n')
 
 
 def next_game() -> bool:
     """
     Функция спрашивает разрешение на следущую игру.
+    Просит пользователя ввести ответ на вопрос в консоль.
     
     Returns
     -------
     bool
         Если играем еще - True, если нет - False
     """
+    result = input('\nХотите попробовать еще? Да[д]\Нет[что-угодно]: ')
+    return True if result == 'д' else False
     
-    result = input('\n' + 'Хотите попробовать еще? Да[д]\Нет[что-угодно]: ')
-    
-    if result == 'д':
-        return True
-    else:
-        return False
-    
-    
+ 
 def tic() -> None:
     """
     Реализаует логику по правилам игры в "Крестики-Нолики".
     
     Инициализируются слудующие перменные:
-        board_matrix - отображение 2D-матрицы в 1D. 3х3=9 элементов. 
-        step -  номер игрового хода
-        last_input - содержит занятые ячейки на игровом поле
-        user_item - "тумблер" переключения игроков.
-        user -  выступает одновременно и как "имя" игрока, и как символ, 
-        которм заполняется выбранное играком поле
+    board_matrix - отображение 2D-матрицы в 1D. SIZE*SIZE элементов. 
+    step -  номер игрового хода
+    last_input - содержит занятые ячейки на игровом поле
+    user_item - "тумблер" переключения игроков.
+    user -  выступает одновременно и как "имя" игрока, и как символ, 
+                          которм заполняется выбранное играком поле
     """
-    
-    board_matrix = [' ' for _ in range(9)]
+    global SIZE
+    board_matrix = [' ' for _ in range(SIZE*SIZE)]
     step = 0
     last_input = set()
     user_item = {'X':'O', 'O':'X'}
@@ -173,14 +190,15 @@ def tic() -> None:
     # Первый ходит Х
     user = user_item['O'] # user = 'X'
     win = False
-
-    while step < 9 and not win:
+    
+    # Всего SIZE*SIZE-1 ходов за игру 
+    while step < SIZE*SIZE and not win:
         
         # Ход пользователя
-        user_1_input = input_value(user, last_input)
+        user_input = input_value(user, last_input)
         
-        last_input.add(user_1_input)
-        board_matrix[user_1_input-1] = user
+        last_input.add(user_input)
+        board_matrix[user_input-1] = user
         print_board(board_matrix)
         step += 1
         
@@ -188,8 +206,8 @@ def tic() -> None:
         win = cheak_win(user, board_matrix, step)
         cheak_pass(step, win)
         
-        #Смена пользователя
-        user = user_item[user] # user_item['X'] = 'O'
+        # Нужно сменить пользователя
+        user = user_item[user]
 
         
 def main() -> None:
